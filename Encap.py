2bbbb89f0b3834597428d799e1bd35ea3b6fb064
31373773 <55849726+31373773@users.noreply.github.com>
2020-03-03 10:37:54 -0500

    Update Encap.py

--- Encap.py
+++ Encap.py
@@ -1,1 +1,19 @@
+class cake:
+    def __init__(self, bakery, flavor):
+        self.bakery = bakery
+        self.__flavor = flavor
 
+    def display(self):
+        print(self.bakery)
+        print(self.__flavor)
+
+    def getflavor(self):
+        print(self.__Bakery)
+
+    def setflavor(self, flavor):
+        self.__flavor = flavor
+
+cake = cake('Carlos Bakery', 'chocolate')
+cake.display()
+cake.getflavor('strawberry')
+cake.setflavor()
